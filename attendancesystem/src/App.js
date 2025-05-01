f9dc996fa189ddfc07c64e56a9c6a26e9f5c06f4
Metan Salih <metansalih@Metans-MacBook-Air.local>
2025-05-01 00:20:22 -0500

    fix: resolve merge conflict in App.js

--- attendancesystem/src/App.js
+++ attendancesystem/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
-<<<<<<< HEAD
 import Register from './components/Register';
-=======
->>>>>>> 49da8b91eb942a5f3d511687146983bc1ec4825f
 import AttendanceHistory from './components/attendance-history';
 import StudentHome from './components/StudentHome';
 import TeacherHome from './components/TeacherHome';
@@ -16,10 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-<<<<<<< HEAD
         <Route path="/register" element={<Register />} />
-=======
->>>>>>> 49da8b91eb942a5f3d511687146983bc1ec4825f
         <Route path="/shome" element={<StudentHome />} />
         <Route path="/thome" element={<TeacherHome />} />
         <Route path="/attendance-history" element={<AttendanceHistory />} />
