0dc6088c730d1be4eb72928a3e6e5fbc5189531f
kocchiwomiro <126615758+kocchiwomiro@users.noreply.github.com>
2025-05-03 12:43:52 -0500

    removed redunant useNavigate for Login.js

--- attendancesystem/src/components/Login.js
+++ attendancesystem/src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
 
 const Login = ({setUsername, role, setRole}) => {
   const navigate = useNavigate();
