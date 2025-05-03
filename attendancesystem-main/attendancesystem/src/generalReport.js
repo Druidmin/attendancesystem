90cd8580c682f1e1606e6c62ae44a7ed2a03d298
Sammi Hamdan <sah9j@mtmail.mtsu.edu>
2025-05-02 23:02:13 -0500

    Reorganized File Structure

--- attendancesystem-main/attendancesystem/src/generalReport.js
+++
@@ -1,6 +0,0 @@
-useEffect(() => {
-  fetch('http://localhost/attendance-system/api/getReport.php')
-    .then(response => response.json())
-    .then(data => setReportData(data))
-    .catch(error => console.error('Error fetching report:', error));
-}, []);
