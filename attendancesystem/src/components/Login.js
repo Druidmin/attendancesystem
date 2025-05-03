5ecb4b5c1ab13734d113ba992becf1d9f9e45935
kocchiwomiro <126615758+kocchiwomiro@users.noreply.github.com>
2025-05-03 12:31:24 -0500

    Added to useNavigate to login.

--- attendancesystem/src/components/Login.js
+++ attendancesystem/src/components/Login.js
@@ -3,6 +3,7 @@ import './Login.css';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: '',
     password: ''
