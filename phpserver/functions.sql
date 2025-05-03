73aed3ab322c4caf7375aa1adf8170c6384c2218
Andrew :) <Acradawg@gmail.com>
2025-05-02 21:02:15 -0500

    New Procedures/End Points

--- phpserver/functions.sql
+++ phpserver/functions.sql
@@ -35,8 +35,28 @@ BEGIN
     WHERE course = course_name AND status = 'present';
 END;
 
+CREATE PROCEDURE get_studentpercentage(IN p_course_name VARCHAR(100), IN p_name VARCHAR(100))
+BEGIN
+    SELECT status, COUNT(*) as count
+    FROM attendance
+    WHERE (course = p_course_name and student = p_name)
+    GROUP BY status;
+END;
+
 CREATE PROCEDURE create_user(IN p_name VARCHAR(100), IN p_username VARCHAR(100), IN p_password VARCHAR(100), IN p_role VARCHAR(50), IN p_course_name VARCHAR(100))
 BEGIN
     INSERT INTO users (name, username, password, role) VALUES (p_name, p_username, p_password, p_role);
     INSERT INTO course_enrollment (student, course) VALUES (p_name, p_course_name);
+END;
+
+CREATE PROCEDURE mark_attendance(IN p_name VARCHAR(100), IN p_course_name VARCHAR(100), IN p_date DATE, IN p_status VARCHAR(10))
+BEGIN
+    INSERT INTO attendance(student, course, date, status) VALUES (p_name, p_course_name, p_date, p_status);
+END;
+
+CREATE PROCEDURE get_user(IN p_username VARCHAR(100), IN p_password VARCHAR(100)) 
+BEGIN
+    SELECT name, role
+    FROM users
+    WHERE (username = p_username and password = p_password);
 END;
