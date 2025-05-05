851a4dace529fa868804fd583d582ff46ee6ad24
Druidmin <apl3h@mtmail.mtsu.edu>
2025-05-05 11:48:20 -0500

    Update App.js

--- attendancesystem/src/App.js
+++ attendancesystem/src/App.js
@@ -6,6 +6,7 @@ import AttendanceHistory from './components/attendance-history';
 import StudentHome from './components/StudentHome';
 import TeacherHome from './components/TeacherHome';
 import ViewAttendanceHistory from './components/view-attendance-history';
+import MarkAttendancePage from './components/MarkAttendancePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/thome" element={<TeacherHome />} />
         <Route path="/attendance-history" element={<AttendanceHistory />} />
         <Route path="/view-attendance-history" element={<ViewAttendanceHistory />} />
+        <Route path="/mark-attendance" element={<MarkAttendancePage/>}/>
       </Routes>
     </Router>
   );
